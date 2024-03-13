12f130fc2f228905c22e2b33836a83a736026af7
Service Web <sw@gssmail.com>
2024-03-13 12:21:11 -0500

    Commit changes in C:\Websites\Git\emarlow\5129\GCG_5129_OpenCust.sql

---
+++ GCG_5129_OpenCust.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_5129_OpenCust
+ * ========================================== */
+CREATE TABLE "GCG_5129_OpenCust"(
+ "Cust" CHAR(6),
+ "User1" CHAR(8),
+ "Populate" LONGVARCHAR);
+
+
