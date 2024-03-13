a2b699126986bcfa285b32dfb1b0d038550ae802
Service Web <sw@gssmail.com>
2024-03-13 12:21:11 -0500

    Commit changes in C:\Websites\Git\emarlow\5129\GCG_5129_CustChanges.sql

---
+++ GCG_5129_CustChanges.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_5129_CustChanges
+ * ========================================== */
+CREATE TABLE "GCG_5129_CustChanges"(
+ "Cust" CHAR(6),
+ "User1" CHAR(8),
+ "ColumnName" CHAR(100),
+ "OldValue" LONGVARCHAR,
+ "NewValue" LONGVARCHAR,
+ "DateChanged" CHAR(50));
+
+
